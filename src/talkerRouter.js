1d931b6aea6e7f68f25da482e65fd8abe1c83722
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-06-21 12:09:37 -0300

    feat: criação do endpoint get /talker/:id

--- src/talkerRouter.js
+++ src/talkerRouter.js
@@ -12,4 +12,17 @@ talkerRouter.get('/', (_req, res) => {
   res.json(parsed).status(200);
 });
 
+talkerRouter.get('/:id', (req, res) => {
+  const data = fs.readFileSync(filePath, 'utf-8');
+  const parsed = JSON.parse(data);
+  
+  const { id } = req.params;
+
+  const talker = parsed.find((t) => t.id === Number(id));
+  
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  
+  res.json(talker).status(200);
+});
+
 module.exports = talkerRouter;
