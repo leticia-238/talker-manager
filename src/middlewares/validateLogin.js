cfa26967215b6a17bca53bc7aac40ca502e82a1b
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-06-21 22:18:18 -0300

    feat: validação do endpoint /login

--- src/middlewares/validateLogin.js
+++ src/middlewares/validateLogin.js
@@ -1,7 +1,23 @@
+class ValidationError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
+
+const emptyEmail = 'O campo "email" é obrigatório';
+const invalidEmail = 'O "email" deve ter o formato "email@email.com"';
+const emptyPassword = 'O campo "password" é obrigatório';
+const invalidPassword = 'O "password" deve ter pelo menos 6 caracteres';
+
 const validateLogin = (req, _res, next) => {
   const { email, password } = req.body;
   
-  if (!email && !password) throw new Error('invalid data');
+  if (!email) throw new ValidationError(emptyEmail);
+  if (!/\S+@\S+\.com/.test(email)) throw new ValidationError(invalidEmail);
+  if (!password) throw new ValidationError(emptyPassword);
+  if (!/\S{6,}/.test(password)) throw new ValidationError(invalidPassword);
+  
   next(); 
 };
 
