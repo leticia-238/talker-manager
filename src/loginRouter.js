39d33bc91273c74859c578fb4f975ecef0d0a13c
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-06-22 21:49:12 -0300

    style: alteração no espaço de identação

--- src/loginRouter.js
+++ src/loginRouter.js
@@ -4,8 +4,8 @@ const validateLogin = require('./middlewares/validateLogin');
 const loginRouter = Router();
 
 loginRouter.post('/', validateLogin, async (_req, res) => {
-    const token = `${Date.now()}${Date.now()}`.slice(0, 16);
-    res.json({ token }).status(200);
+  const token = `${Date.now()}${Date.now()}`.slice(0, 16);
+  res.json({ token }).status(200);
 });
 
 module.exports = loginRouter;
